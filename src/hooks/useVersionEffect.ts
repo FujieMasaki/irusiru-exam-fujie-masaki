1f26a12d3ff36b0ddab1f61dafe586d69cbde27a
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2025-02-21 10:01:16 +0900

    問題⑤ バージョン更新時の無限レンダリングを防止

--- src/hooks/useVersionEffect.ts
+++ src/hooks/useVersionEffect.ts
@@ -1,18 +1,21 @@
-import { useRecoilValue } from "recoil"
-import { currentSlide, CurrentSlideManager } from "../recoil/currentSlide"
-import { useEffect } from "react"
+import { useRecoilValue } from "recoil";
+import { currentSlide, CurrentSlideManager } from "../recoil/currentSlide";
+import { useEffect } from "react";
 
 export const useVersionEffect = () => {
-    const _currentSlide = useRecoilValue(currentSlide)
+  const _currentSlide = useRecoilValue(currentSlide);
 
-    const handleSetVersion = () => CurrentSlideManager.aaa(prev => ({
-        ...prev,
-        version: prev.version + 1
-      }))
-    
-    useEffect(() => { 
-        handleSetVersion()
-      }, [_currentSlide])
-      
-    return null;
-}
+  const handleSetVersion = () =>
+    CurrentSlideManager.aaa((prev) => ({
+      ...prev,
+      version: prev.version + 1,
+    }));
+
+  useEffect(() => {
+    if (_currentSlide.items.length > 0) {
+      handleSetVersion();
+    }
+  }, [_currentSlide.items]);
+
+  return null;
+};
