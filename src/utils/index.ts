32894028af4bb7655b1c440bf8aa791a77211a3b
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2025-02-21 10:38:19 +0900

    問題①不要import削除

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,3 @@
-import { isEqual } from "lodash";
-import { useRef, useState } from "react";
-
 type Falsy = false | "" | 0 | null | undefined;
 // 問題① 適切な命名に変更してください
 export const filterOutFalsy = <T>(arr: T[]): Exclude<T, Falsy>[] => {
