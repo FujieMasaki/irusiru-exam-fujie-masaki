40b38e723ec95b85e9a7dfd02b73b8923668255f
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2025-02-21 10:37:54 +0900

    問題① func1から適切な命名に変更

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
 import { isEqual } from "lodash";
 import { useRef, useState } from "react";
 
-type Falsy = false | "" | 0 | null | undefined; 
+type Falsy = false | "" | 0 | null | undefined;
 // 問題① 適切な命名に変更してください
-export const func1 = <T>(arr: T[]): Exclude<T, Falsy>[] => {
+export const filterOutFalsy = <T>(arr: T[]): Exclude<T, Falsy>[] => {
   return arr.filter((item) => Boolean(item)) as Exclude<T, Falsy>[];
-}
+};
